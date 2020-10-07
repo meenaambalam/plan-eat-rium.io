7c79409eb356d3cdcf124be557ce6b8d39bc7859
Grantjos12 <grantjoslyn@gmail.com>
2020-10-07 13:17:45 -0500

    2nd API

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 var queryURL =
   "https://api.edamam.com/search?q=chicken&app_id=2ae93fb0&app_key=2438d9ac01f49f4467b8f7013aae8da6";
-var queryURL = "https://api.edamam.com/search?q=chicken+beef+corn&app_id=2ae93fb0&app_key=2438d9ac01f49f4467b8f7013aae8da6"	
-//var queryURL = "https://api.edamam.com/search?q=" + searchcontenthere + "&app_id=2ae93fb0&app_key=2438d9ac01f49f4467b8f7013aae8da6"	
 
-var recipeName = $("#testme2")
-var ingredients = $("#testme")
-var recipeName2 = $("#testme3")
-var ingredients2 = $("#testme4")
-var recipeName3 = $("#testme5")
-var ingredients3 = $("#testme6")
-var image1 = $("#testme7")
-var image2 = $("#testme8")
-var image3 = $("#testme9")
-var link1 = $("testme10")
-var link2 = $("testme11")
-var link3 = $("testme12")
-
+var recipeName = $("#testme2");
+var ingredients = $("#testme");
+var recipeName2 = $("#testme3");
+var ingredients2 = $("#testme4");
+var recipeName3 = $("#testme5");
+var ingredients3 = $("#testme6");
+var image1 = $("#testme7");
+var image2 = $("#testme8");
+var image3 = $("#testme9");
+var link1 = $("testme10");
+var link2 = $("testme11");
+var link3 = $("testme12");
 
 var recipeName = $("#testme2");
 var ingredients = $("#testme");
@@ -24,10 +21,9 @@ var ingredients2 = $("#testme4");
 var recipeName3 = $("#testme5");
 var ingredients3 = $("#testme6");
 
-var APIkey = "2438d9ac01f49f4467b8f7013aae8da6"
+var APIkey = "2438d9ac01f49f4467b8f7013aae8da6";
 
 //var searchBar = $("<li> <button id=\"tester\"class=\"btn btn-secondary\">" + citySearch.val() + "</button></li>")
-
 
 $.ajax({
   url: queryURL,
@@ -50,71 +46,95 @@ $.ajax({
 });
 
 //setting input to local storage
-localStorage.setItem("ingrents");
-
-//getting item from local storage and pushing it to the array
-// grabbing input and button
-// storing string to local storage
 
 //included updated id's for acessing dom elemnts
-$("aside div??").click(function () {
-  for (var i = 0; i < ingredients.length; i++) {
-    currentIng = ingredents[i] + $("<button>test button</button>");
-    currentIng.text("arrayvalue"); //add array value
-    currentIng.click(function () {});
+
+// $("aside div??").click(function () {
+//   for (var i = 0; i < ingredients.length; i++) {
+//     currentIng = ingredents[i] + $("<button>test button</button>");
+//     currentIng.text("arrayvalue"); //add array value
+//     currentIng.click(function () {});
+//   }
+// });
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  var recipe = JSON.stringify(response.hits[0].recipe.label);
+  var list = response.hits[0].recipe.ingredientLines;
+  var imageURL = response.hits[0].recipe.image;
+  var imageBox1 = $("<img src='" + imageURL + "'>'");
+  image1.append(imageBox1);
+  console.log(list);
+
+  for (var i = 0; i < list.length; i++) {
+    var someingredient = $("<li>" + list[i] + "</li>");
+    ingredients.append(someingredient);
   }
+
+  recipeName.text(recipe);
+  var recipe2 = JSON.stringify(response.hits[1].recipe.label);
+  var list2 = response.hits[1].recipe.ingredientLines;
+
+  console.log(list2);
+  for (var i = 0; i < list2.length; i++) {
+    var someingredient2 = $("<li>" + list2[i] + "</li>");
+    ingredients2.append(someingredient2);
+  }
+
+  var imageURL2 = response.hits[1].recipe.image;
+  var linkURL2 = response.hits[1].recipe.url;
+  var source2 = $("<a href='" + linkURL2 + "'>");
+  link2.append(source2);
+  var imageBox2 = $("<img src='" + imageURL2 + "'>'");
+  image2.append(imageBox2);
+
+  recipeName2.text(recipe2);
+  var recipe3 = JSON.stringify(response.hits[2].recipe.label);
+  var list3 = response.hits[2].recipe.ingredientLines;
+  for (var i = 0; i < list3.length; i++) {
+    var someingredient2 = $("<li>" + list3[i] + "</li>");
+    ingredients3.append(someingredient2);
+  }
+
+  var imageURL3 = response.hits[2].recipe.image;
+  var imageBox3 = $("<img src='" + imageURL3 + "'>'");
+  image3.append(imageBox3);
+  recipeName3.text(recipe3);
 });
-s;
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-    var recipe = JSON.stringify(response.hits[0].recipe.label)
-    var list = (response.hits[0].recipe.ingredientLines)
-    var imageURL = (response.hits[0].recipe.image)
-    var imageBox1 = $("<img src='" + imageURL + "'>'");
-    image1.append(imageBox1)
-    console.log(list)
 
-    for (var i=0; i < list.length; i++) {
-        var someingredient = $("<li>" + list[i] + "</li>")
-        ingredients.append(someingredient)
-    }
-    
-    recipeName.text(recipe)
-    var recipe2 = JSON.stringify(response.hits[1].recipe.label)
-    var list2 = (response.hits[1].recipe.ingredientLines)
+//mealDB and APi key ~ 1
 
-    console.log(list2)
-    for (var i=0; i < list2.length; i++) {
-        var someingredient2 = $("<li>" + list2[i] + "</li>")
-        ingredients2.append(someingredient2)}
+var mealURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"; //ingredients
+var mealURLthree =
+  "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"; //list of instructions //str instructions
+//www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview //this is for a preview image
 
+//call to second API
+https: $.ajax({
+  url: mealURLthree,
+  method: "GET",
+}).then(function (responseTwo) {
+  console.log(responseTwo);
+});
+////////////
 
+//match recipes to calories //spoonacular api//=q=chicken&apiKey
+var mealURLspoon =
+  "https://api.spoonacular.com/recipes/chicken/information?=q=chicken&apiKey=571ff79b4eb14a0fbc553ed4db1d63c4&includeNutrition=true";
+("https://api.spoonacular.com/recipes/chicken/information?=q=chicken&apiKey=571ff79b4eb14a0fbc553ed4db1d63c4&includeNutrition=true");
 
-    var imageURL2 = (response.hits[1].recipe.image)
-    var linkURL2 = (response.hits[1].recipe.url)
-    var source2 = $("<a href='" + linkURL2 + "'>")
-    link2.append(source2)
-    var imageBox2 = $("<img src='" + imageURL2 + "'>'");
-    image2.append(imageBox2)
-    
-    recipeName2.text(recipe2)
-    var recipe3 = JSON.stringify(response.hits[2].recipe.label)
-    var list3 = (response.hits[2].recipe.ingredientLines)
-    for (var i=0; i < list3.length; i++) {
-        var someingredient2 = $("<li>" + list3[i] + "</li>")
-        ingredients3.append(someingredient2)}
+// "https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2";
 
-    var imageURL3 = (response.hits[2].recipe.image)
-    var imageBox3 = $("<img src='" + imageURL3 + "'>'");
-    image3.append(imageBox3)
-    recipeName3.text(recipe3)
-    
+// "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
 
+https: https: var APIspoon = "571ff79b4eb14a0fbc553ed4db1d63c4";
 
-
-
-
-
-})
+https: $.ajax({
+  url: mealURLspoon,
+  method: "GET",
+}).then(function (responseThree) {
+  console.log(responseThree);
+});
