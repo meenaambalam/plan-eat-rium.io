786808613208b1da5a9524f1fbdcf9c38a3d52a7
Grantjos12 <grantjoslyn@gmail.com>
2020-10-07 19:39:12 -0500

    added IMG and TITlE to HTMl via second API

--- script.js
+++ script.js
@@ -1,135 +1,158 @@
+var recipeName = $("#testme2");
+var ingredients = $("#testme");
+var recipeName2 = $("#testme3");
+var ingredients2 = $("#testme4");
+var recipeName3 = $("#testme5");
+var ingredients3 = $("#testme6");
+var image1 = $("#testme7");
+var image2 = $("#testme8");
+var image3 = $("#testme9");
+var spoonApi = $("#second-api");
+var link1 = $("testme10");
+var link2 = $("testme11");
+var link3 = $("testme12");
+var search1 = $("#Ingredient1");
+var search2 = $("#Ingredient2");
+var search3 = $("#Ingredient3");
 
-var recipeName = $("#testme2")
-var ingredients = $("#testme")
-var recipeName2 = $("#testme3")
-var ingredients2 = $("#testme4")
-var recipeName3 = $("#testme5")
-var ingredients3 = $("#testme6")
-var image1 = $("#testme7")
-var image2 = $("#testme8")
-var image3 = $("#testme9")
-var link1 = $("testme10")
-var link2 = $("testme11")
-var link3 = $("testme12")
-var search1 = $("#Ingredient1")
-var search2 = $("#Ingredient2")
-var search3 = $("#Ingredient3")
+var APIkey = "2438d9ac01f49f4467b8f7013aae8da6";
 
-var APIkey = "2438d9ac01f49f4467b8f7013aae8da6"
+$("#search-recipes").on(
+  "click",
 
-$("#search-recipes").on("click", 
+  function runSearch() {
+    var queryURL =
+      "https://api.edamam.com/search?q=" +
+      search1.val().trim() +
+      "+" +
+      search2.val().trim() +
+      "+" +
+      search3.val().trim() +
+      "&app_id=2ae93fb0&app_key=2438d9ac01f49f4467b8f7013aae8da6";
 
-function runSearch() {
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var recipe = JSON.stringify(response.hits[0].recipe.label);
+      var list = response.hits[0].recipe.ingredientLines;
+      var imageURL = response.hits[0].recipe.image;
+      var imageBox1 = $("<img src='" + imageURL + "'>'");
+      image1.append(imageBox1);
+      console.log(list);
 
-var queryURL = "https://api.edamam.com/search?q=" + search1.val().trim() + "+" + search2.val().trim() + "+" + search3.val().trim() + "&app_id=2ae93fb0&app_key=2438d9ac01f49f4467b8f7013aae8da6"	
-
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-  var recipe = JSON.stringify(response.hits[0].recipe.label);
-  var list = response.hits[0].recipe.ingredientLines;
-  var imageURL = response.hits[0].recipe.image;
-  var imageBox1 = $("<img src='" + imageURL + "'>'");
-  image1.append(imageBox1);
-  console.log(list);
-
-  for (var i = 0; i < list.length; i++) {
-    var someingredient = $("<li>" + list[i] + "</li>");
-    ingredients.append(someingredient);
-  }
-
-  recipeName.text(recipe);
-  var recipe2 = JSON.stringify(response.hits[1].recipe.label);
-  var list2 = response.hits[1].recipe.ingredientLines;
-
-  console.log(list2);
-  for (var i = 0; i < list2.length; i++) {
-    var someingredient2 = $("<li>" + list2[i] + "</li>");
-    ingredients2.append(someingredient2);
-  }
-
-  var imageURL2 = response.hits[1].recipe.image;
-  var linkURL2 = response.hits[1].recipe.url;
-  var source2 = $("<a href='" + linkURL2 + "'>");
-  link2.append(source2);
-  var imageBox2 = $("<img src='" + imageURL2 + "'>'");
-  image2.append(imageBox2);
-
-  recipeName2.text(recipe2);
-  var recipe3 = JSON.stringify(response.hits[2].recipe.label);
-  var list3 = response.hits[2].recipe.ingredientLines;
-  for (var i = 0; i < list3.length; i++) {
-    var someingredient2 = $("<li>" + list3[i] + "</li>");
-    ingredients3.append(someingredient2);
-  }
-
-  var imageURL3 = response.hits[2].recipe.image;
-  var imageBox3 = $("<img src='" + imageURL3 + "'>'");
-  image3.append(imageBox3);
-  recipeName3.text(recipe3);
-
-  // function to handle Recipe Button "Click" event
-  $(".days").on("click", function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var day = $(this).val();
-
-    for (var i = 1; i < 4; i++) {
-      var chkRecipe = '(".' + day + i + '")';
-      if ($("." + day + i).text() === "Recipe Name") {
-        $("." + day + i).removeClass("teal");
-        $("." + day + i).addClass("blue");
-        $("." + day + i).text("Recipe from Carousel");
-        $("." + day + i).attr(
-          "href",
-          "https://www.epicurious.com/recipes/food/views/irish-channel-corned-beef-and-cabbage-51224220"
-        );
-        break;
+      for (var i = 0; i < list.length; i++) {
+        var someingredient = $("<li>" + list[i] + "</li>");
+        ingredients.append(someingredient);
       }
-    }
-  });
 
-  //function to handle Delete Button "Click" event to clear the color, recipe name and href information
+      recipeName.text(recipe);
+      var recipe2 = JSON.stringify(response.hits[1].recipe.label);
+      var list2 = response.hits[1].recipe.ingredientLines;
 
-  $(".delBtn").on("click", function (event) {
-    event.preventDefault();
-    event.stopPropagation();
+      console.log(list2);
+      for (var i = 0; i < list2.length; i++) {
+        var someingredient2 = $("<li>" + list2[i] + "</li>");
+        ingredients2.append(someingredient2);
+      }
 
-    var delBtnVal = $(this).val();
-    $("#recipeBtn" + delBtnVal).text("Recipe Name");
-    $("#recipeBtn" + delBtnVal).removeClass("blue");
-    $("#recipeBtn" + delBtnVal).addClass("teal");
-    $("#recipeBtn" + delBtnVal).attr("href", "#");
-  });
-});
-//mealDB and APi key ~ 1
+      var imageURL2 = response.hits[1].recipe.image;
+      var linkURL2 = response.hits[1].recipe.url;
+      var source2 = $("<a href='" + linkURL2 + "'>");
+      link2.append(source2);
+      var imageBox2 = $("<img src='" + imageURL2 + "'>'");
+      image2.append(imageBox2);
 
-var mealURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"; //ingredients
-var mealURLthree =
-  "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"; //list of instructions //str instructions
-//www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview //this is for a preview image
+      recipeName2.text(recipe2);
+      var recipe3 = JSON.stringify(response.hits[2].recipe.label);
+      var list3 = response.hits[2].recipe.ingredientLines;
+      for (var i = 0; i < list3.length; i++) {
+        var someingredient2 = $("<li>" + list3[i] + "</li>");
+        ingredients3.append(someingredient2);
+      }
 
-//call to second API
-https: $.ajax({
-  url: mealURLthree,
-  method: "GET",
-}).then(function (responseTwo) {
-  console.log(responseTwo);
-});
+      var imageURL3 = response.hits[2].recipe.image;
+      var imageBox3 = $("<img src='" + imageURL3 + "'>'");
+      image3.append(imageBox3);
+      recipeName3.text(recipe3);
 
+      // function to handle Recipe Button "Click" event
+      $(".days").on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        var day = $(this).val();
 
-//match recipes to calories //spoonacular api//=q=chicken&apiKey
-var mealURLspoon =
-  "https://api.spoonacular.com/recipes/chicken/information?=q=chicken&apiKey=571ff79b4eb14a0fbc553ed4db1d63c4&includeNutrition=true"; //working
-("https://api.spoonacular.com/recipes/chicken/information?=q=chicken&apiKey=571ff79b4eb14a0fbc553ed4db1d63c4&includeNutrition=true");
+        for (var i = 1; i < 4; i++) {
+          var chkRecipe = '(".' + day + i + '")';
+          if ($("." + day + i).text() === "Recipe Name") {
+            $("." + day + i).removeClass("teal");
+            $("." + day + i).addClass("blue");
+            $("." + day + i).text("Recipe from Carousel");
+            $("." + day + i).attr(
+              "href",
+              "https://www.epicurious.com/recipes/food/views/irish-channel-corned-beef-and-cabbage-51224220"
+            );
+            break;
+          }
+        }
+      });
 
-var APIspoon = "571ff79b4eb14a0fbc553ed4db1d63c4";
+      //function to handle Delete Button "Click" event to clear the color, recipe name and href information
 
-https: $.ajax({
-  url: mealURLspoon,
-  method: "GET",
-}).then(function (responseThree) {
-  console.log(responseThree);
-});
+      $(".delBtn").on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var delBtnVal = $(this).val();
+        $("#recipeBtn" + delBtnVal).text("Recipe Name");
+        $("#recipeBtn" + delBtnVal).removeClass("blue");
+        $("#recipeBtn" + delBtnVal).addClass("teal");
+        $("#recipeBtn" + delBtnVal).attr("href", "#");
+      });
+    });
+    //mealDB and APi key ~ 1
+
+    // var mealURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"; //ingredients
+    // var mealURLthree =
+    //   "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"; //list of instructions //str instructions
+    // //www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview //this is for a preview image
+
+    // //call to second API
+    // https: $.ajax({
+    //   url: mealURLthree,
+    //   method: "GET",
+    // }).then(function (responseTwo) {
+    //   console.log(responseTwo);
+    // });
+
+    //match recipes to calories //spoonacular api//=q=chicken&apiKey
+
+    var spoonURL =
+      "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
+      search1.val().trim() +
+      "+" +
+      search2.val().trim() +
+      "+" +
+      search3.val().trim() +
+      "&apiKey=571ff79b4eb14a0fbc553ed4db1d63c4";
+
+    var APIspoon = "571ff79b4eb14a0fbc553ed4db1d63c4";
+
+    https: $.ajax({
+      url: spoonURL,
+      method: "GET",
+    }).then(function (responseThree) {
+      console.log(responseThree);
+      console.log(responseThree[1].title);
+      console.log(responseThree[1].image); //image
+      var imgSpoon = responseThree[1].image;
+      var titleSpoon = JSON.stringify(responseThree[1].title);
+      var spoonTag = $("<img src='" + imgSpoon + "'>'");
+      var spoonTitle = $("<p>" + titleSpoon + "<p>");
+      spoonApi.html(spoonTitle);
+      // spoonApi.text(titleSpoon);
+      spoonApi.append(spoonTag);
+    });
+  }
+);
